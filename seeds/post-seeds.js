dce246056355a1a7f713fedbcf3023a4c303fcc9
fryylord <rob@mchays.com>
2022-12-18 17:43:59 -0600

    fixed post seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,32 +3,32 @@ const { Post } = require('../models');
 const postData = [
   {
     title: 'Test Post 1',
-    post_text: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
+    post_content: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
     user_id: 1,
   },
   {
     title: 'Test Post 2',
-    post_text: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
+    post_content: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
     user_id: 2,
   },
   {
     title: 'Test Post 3',
-    post_text: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
+    post_content: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
     user_id: 2,
   },
   {
     title: 'Test Post 4',
-    post_text: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
+    post_content: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
     user_id: 3,
   },
   {
     title: 'Test Post 5',
-    post_text: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
+    post_content: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
     user_id: 4,
   },
   {
     title: 'Test Post 6',
-    post_text: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
+    post_content: 'atiojoin aoifsjdn aoisfjnd ionoailfdsn naoilfsd',
     user_id: 5,
   },
 ]
