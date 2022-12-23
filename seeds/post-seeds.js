d0d535932bf45067b46a2f3292323b520757f2e3
fryylord <rob@mchays.com>
2022-12-22 18:23:09 -0600

    reduced seed amount

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,34 +2,10 @@ const { Post } = require('../models');
 
 const postData = [
   {
-    title: 'Test Post Title 1',
-    post_content: 'Test Post Text 1',
+    title: 'Welcome to this Tech Blog',
+    post_content: 'You can click on the post title to expand the post and see comments',
     user_id: 1,
-  },
-  {
-    title: 'Test Post Title 2',
-    post_content: 'Test Post Text 2',
-    user_id: 2,
-  },
-  {
-    title: 'Test Post Title 3',
-    post_content: 'Test Post Text 3',
-    user_id: 2,
-  },
-  {
-    title: 'Test Post Title 4',
-    post_content: 'Test Post Text 4',
-  },
-  {
-    title: 'Test Post Title 5',
-    post_content: 'Test Post Text 5',
-    user_id: 4,
-  },
-  {
-    title: 'Test Post Title 6',
-    post_content: 'Test Post Text 6',
-    user_id: 5,
-  },
+  }
 ]
 
 const seedPosts = () => Post.bulkCreate(postData);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,28 +2,8 @@ const { User } = require('../models');
 
 const userData = [
   {
-    username: "TestUserOne",
-    email: "TestUserOne@gmail.com",
-    password: "password1234"
-  },
-  {
-    username: "TestUserTwo",
-    email: "TestUserTwo@gmail.com",
-    password: "password1234"
-  },
-  {
-    username: "TestUserThree",
-    email: "TestUserThree@gmail.com",
-    password: "password1234"
-  },
-  {
-    username: "TestUserFour",
-    email: "TestUserFour@gmail.com",
-    password: "password1234"
-  },
-  {
-    username: "TestUserFive",
-    email: "TestUserFive@gmail.com",
+    username: "admin",
+    email: "admin@gmail.com",
     password: "password1234"
   }
 ];
